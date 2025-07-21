b812b5d791124814992f3dc7746706fa0dc1f4eb
YuvaneshSankar <yuvanesh.skv@gmail.com>
2025-07-22 04:07:02 +0530

    Finished testing

--- convertor.py
+++ convertor.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import numpy as np
 
-# Load the Excel file
+
 file_path = 'data.xlsx'
 
-# Read original matrix from Sheet1
+
 matrix_df = pd.read_excel(file_path, sheet_name='Sheet1', header=None)
 
-# Read conversion data from Sheet2
+
 conversion_df = pd.read_excel(file_path, sheet_name='Sheet2')
 
-# Prepare lower AHP conversion table
+
 lower_conversion = conversion_df[['Data Chart for lower AHP', 'Unnamed: 1']].iloc[1:].dropna()
 lower_conversion.columns = ['Present_Value', 'Final_Value']
 lower_conversion['Present_Value'] = lower_conversion['Present_Value'].astype(float)
 lower_conversion['Final_Value'] = lower_conversion['Final_Value'].astype(float)
 
-# Prepare upper AHP conversion table
+
 upper_conversion = conversion_df[['Data Chart for upper AHP', 'Unnamed: 4']].iloc[1:].dropna()
 upper_conversion.columns = ['Present_Value', 'Final_Value']
 upper_conversion['Present_Value'] = upper_conversion['Present_Value'].astype(float)
 upper_conversion['Final_Value'] = upper_conversion['Final_Value'].astype(float)
 
-# Create mapping dictionaries for quick lookup
+
 lower_map = dict(zip(lower_conversion['Present_Value'], lower_conversion['Final_Value']))
 upper_map = dict(zip(upper_conversion['Present_Value'], upper_conversion['Final_Value']))
 
@@ -32,16 +32,16 @@ def map_values(matrix, conv_map):
     for i in range(matrix.shape[0]):
         for j in range(matrix.shape[1]):
             val = matrix.iat[i, j]
-            # Find the closest key in the conversion map
+            
             closest_key = min(sorted_keys, key=lambda x: abs(x-val))
             mapped_matrix.iat[i, j] = conv_map[closest_key]
     return mapped_matrix
 
-# Generate Lower and Upper AHP matrices
+
 lower_ahp_matrix = map_values(matrix_df, lower_map)
 upper_ahp_matrix = map_values(matrix_df, upper_map)
 
-# Display the output
+
 print('Lower AHP Matrix:')
 print(lower_ahp_matrix)
 print('\nUpper AHP Matrix:')
